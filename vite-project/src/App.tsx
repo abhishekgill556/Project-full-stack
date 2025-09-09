9090189b1e72623aaa692210a086483fc4a443eb
rrcsmehta3 <smehta3@academic.rrc.ca>
2025-09-09 15:30:56 -0500

    Added footer component

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+
 
 import './App.css'
 import Header from "./Components/Header/Header"
+import Footer from './Components/Footer/footer'
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
@@ -11,20 +12,9 @@ function App() {
        
       <Header/>
 
-        
+      <Footer/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </>
   )
 }
