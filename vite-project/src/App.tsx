bdfd8daf0096e89d6ca52d6e5a4746b76f378ab8
Prabhkiran singh <prabhkiransingh@rrc.ca>
2025-09-11 13:19:33 -0500

    modify app.tsx

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,14 +1,14 @@
 import './App.css'
 import Blog from './Components/blog/Blog'
 import Footer from './Components/Footer/footer'
-import Booking from './Components/book/book'
+import Book from './Components/book/book'
 function App() {
  
  
   return (
     <>
       <div>
-        <Header/>
+        <Book/>
         <Blog/>
         <Footer/> 
       </div>
