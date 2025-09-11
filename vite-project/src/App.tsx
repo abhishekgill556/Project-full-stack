c5f494ce62417bc3fbe5f7ff6c379260dc46359f
Prabhkiran singh <prabhkiransingh@rrc.ca>
2025-09-11 13:13:10 -0500

    changes in app.tsx file

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css'
-import Header from "./Components/Header/Header"
 import Blog from './Components/blog/Blog'
 function App() {
   
@@ -7,7 +6,6 @@ function App() {
   return (
     <>
       <div>
-        <Header/>
         <Blog/>
       </div>
       
