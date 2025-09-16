8e25f42ee050d27e6612a22966bd59c838e9bf3f
Prabhkiran singh <prabhkiransingh@rrc.ca>
2025-09-16 14:44:40 -0500

    adding my files

--- vite-project/src/Components/blog/Blog.tsx
+++ vite-project/src/Components/blog/Blog.tsx
@@ -1,39 +1,17 @@
 import "./Blog.css";
+import { blogPosts } from "./data/blogData";
 
 export default function Blog() {
   return (
-    <section id="blog" className="blog">
+    <section className="blog">
       <h2>Our Blog</h2>
-      <article className="blog-post"
-      >
-        <div className="blog-content">
-      <h3>Top 5 Hair Care Tips</h3>
-      <p>
-        Healthy hair starts with the right care routine. Learn how to maintain
-        strong, shiny, and beautiful hair with these expert tips.
-      </p>
-      <a href="#">Read More</a>
-    </div>
-      </article>
-
-      <article className="blog-post"
-      >
-        <h3>Why Extensions are Trending</h3>
-        <p>
-          Hair extensions are becoming more popular than ever. Discover why
-          they’re a must-have for modern styles.
-        </p>
-        <a href="#">Read More</a>
-      </article>
-
-      <article className="blog-post"      >
-        <h3>Wedding Hairstyles 2025</h3>
-        <p>
-          From elegant buns to glamorous curls, explore the top wedding
-          hairstyles that will make your big day unforgettable.
-        </p>
-        <a href="#">Read More</a>
-      </article>
+      {blogPosts.map((x) => (
+        <article key={x.id} className="blog-post">
+          <h3>{x.title}</h3>
+          <p>{x.description}</p>
+          <a href={x.link}>Read More</a>
+        </article>
+      ))}
     </section>
   );
 }
---
+++ vite-project/src/Components/blog/data/blogData.ts
@@ -0,0 +1,25 @@
+import type { BlogPost } from "../type/blogpost";
+
+export const blogPosts: BlogPost[] = [
+  {
+    id: 1,
+    title: "Top 5 Hair Care Tips",
+    description:
+      "Healthy hair starts with the right care routine. Learn how to maintain strong, shiny, and beautiful hair with these expert tips.",
+    link: "#",
+  },
+  {
+    id: 2,
+    title: "Why Extensions are Trending",
+    description:
+      "Hair extensions are becoming more popular than ever. Discover why they’re a must-have for modern styles.",
+    link: "#",
+  },
+  {
+    id: 3,
+    title: "Wedding Hairstyles 2025",
+    description:
+      "From elegant buns to glamorous curls, explore the top wedding hairstyles that will make your big day unforgettable.",
+    link: "#",
+  },
+];
