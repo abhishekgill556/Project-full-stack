4232445970478762d39b2052268a0110335d164d
jasleen <jasleenkaurkalsi03@gmail.com>
2025-09-15 21:50:32 -0500

    added the services and components.

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,14 +1,15 @@
 import './App.css'
 import Header from "./Components/Header/Header"
+import Services from './Components/Services/Services'
 function App() {
  
  
   return (
     <>
       <div>
+      <Services/>
        
       <Header/>
-
       </div>
      
     </>
