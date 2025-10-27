1b0b6140793b32ab23894b4301e0a4d805442a2b
rrcsmehta3 <smehta3@academic.rrc.ca>
2025-10-26 23:38:25 -0500

    Fixed errors

--- src/repo/reviewsRepository.ts
+++ src/repo/reviewsRepository.ts
@@ -5,10 +5,11 @@ let reviews: Review[] = [...(initialReviews as Review[])];
  
 export const reviewRepository = {
   async getAll(): Promise<Review[]> {
-    await new Promise((res) => setTimeout(res, 100)); // simulate delay
+    await new Promise((res) => setTimeout(res, 100)); 
     return [...reviews];
   },
  
+
   async create(review: Review): Promise<void> {
     await new Promise((res) => setTimeout(res, 100));
     reviews = [review, ...reviews];
