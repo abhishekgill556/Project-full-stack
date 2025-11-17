d22877795b4272d4c07f4f11041af54c03e137fc
rrcsmehta3 <smehta3@academic.rrc.ca>
2025-11-17 01:15:41 -0600

    Updated everything and solved the issues

--- backend/prisma/migrations/20251116080333_add_stylist_model/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Stylist" (
-    "id" SERIAL NOT NULL,
-    "service" TEXT NOT NULL,
-    "level" TEXT NOT NULL,
-    "price" DOUBLE PRECISION NOT NULL,
-
-    CONSTRAINT "Stylist_pkey" PRIMARY KEY ("id")
-);
--- backend/prisma/migrations/20251116083436_add_full_stylist_model/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `category` to the `Stylist` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Stylist" ADD COLUMN     "category" TEXT NOT NULL;
